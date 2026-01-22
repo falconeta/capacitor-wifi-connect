f911f136ee76fd2c11eed06af19b8b9ffbc1dfd0
Vittorio Lo Preiato <36620514+falconeta@users.noreply.github.com>
2026-01-22 17:29:49 +0100

    docs: :art: update semantic release

--- release.config.js
+++ release.config.js
@@ -5,7 +5,7 @@ module.exports = {
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
     '@semantic-release/changelog',
-    ['@semantic-release/npm', { npmPublish: true, pkgRoot: '.' }],
+    // ['@semantic-release/npm', { npmPublish: true, pkgRoot: '.' }],
     [
       '@semantic-release/exec',
       {
